9001017acf5a47ee6e0c6e5e14bda462018a34d2
enzo <enzos.yamakawa@gmail.com>
2026-03-13 16:38:30 -0300

    front pegando os animais da lista e colocando na li

--- AppAnimais2/public/index.js
+++ AppAnimais2/public/index.js
@@ -1,6 +1,23 @@
-function carregarAnimais(){
-    axios.get("http://localhost:8000/animais")
-    .then(response => console.log(response.data))
+async function carregarAnimais(){
+    // axios.get("http://localhost:8000/animais")
+    // .then(response => console.log(response.data))
+
+    const response = await axios.get("http://localhost:8000/animais")
+    // precisa ser async para usar await
+
+    const animais = response.data
+
+    const lista = document.getElementById('lista_animais') //transformando o <ul> em uma variável, pegando pelo id
+    
+    animais.forEach(animal => {
+
+        const item = document.createElement('li')
+        item.innerText = animal.nome
+
+        lista.appendChild(item)    
+    });    
+
+    
 
 }
 
